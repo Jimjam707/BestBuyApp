60c56095e0ca66b926e51464688928d3faf23e00
Jimi Adams <jimi-8@live.com>
2026-01-07 08:42:56 -0500

    Completed product viewing

--- BestBuyApp/obj/Debug/net8.0/BestBuyApp.AssemblyInfo.cs
+++ BestBuyApp/obj/Debug/net8.0/BestBuyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BestBuyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a286f6db97513e61093d0ca2ee4f63f5cb144bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BestBuyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BestBuyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

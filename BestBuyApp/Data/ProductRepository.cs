80e041c807c8fe6ec59632f4b2a0d2e575c397d4
Jimi Adams <jimi-8@live.com>
2026-01-16 10:26:09 -0500

    Added DeleteProduct

--- BestBuyApp/Data/ProductRepository.cs
+++ BestBuyApp/Data/ProductRepository.cs
@@ -50,4 +50,14 @@ public class ProductRepository : IProductRepository
         product.Categories = categoryList;
         return product;
     }
+
+    
+
+    public void DeleteProduct(Product product)
+    {
+        _connection.Execute("DELEte FROM REVIEWS WHERE ProductID = @id", new { id = product.ProductID });
+        _connection.Execute("DELETE FROM Sales WHERE ProductID = @id", new { id = product.ProductID });
+        _connection.Execute("DELETE FROM products WHERE ProductID = @id", new { id = product.ProductID });
+        
+    }
 }
